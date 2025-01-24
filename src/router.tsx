b587292c1704ef45cb322dbae8f34df418144795
Daewoong Bang <daewoong.bang@gmail.com>
2025-01-25 01:27:54 +0900

    Add gh-pages

--- src/router.tsx
+++ src/router.tsx
@@ -8,33 +8,38 @@ import CoinDetail from 'components/coin/Detail';
 import CoinChart from 'components/coin/Chart';
 import CoinPrice from 'components/coin/Price';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+        {
+          path: 'coin',
+          element: <CoinPage />,
+          children: [
+            { path: '', element: <CoinList /> },
+            {
+              path: ':coinId',
+              element: <CoinDetail />,
+              children: [
+                { path: 'chart', element: <CoinChart /> },
+                { path: 'price', element: <CoinPrice /> },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: 'coin',
-        element: <CoinPage />,
-        children: [
-          { path: '', element: <CoinList /> },
-          {
-            path: ':coinId',
-            element: <CoinDetail />,
-            children: [
-              { path: 'chart', element: <CoinChart /> },
-              { path: 'price', element: <CoinPrice /> },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]);
+    basename: '/react-crypto-tracker',
+  }
+);
 
 export default router;
